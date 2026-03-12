cdfb21083546550003dd66d863c83f6976534564
luna <rokdoombeast@gmail.com>
2026-03-12 23:03:33 +0000

    made hits UI

--- UpTheSlope/Assets/Scripts/HitCounter.cs
+++ UpTheSlope/Assets/Scripts/HitCounter.cs
@@ -5,30 +5,19 @@ using System;
 
 public class HitCounter : MonoBehaviour
 {
-	// public TextMeshProUGUI hitsText;
-	private int hitCounter;
+	public TextMeshProUGUI HitsText;
+	public string HitsBaseText = "hits: ";
+	private int _hitCounter;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-		hitCounter = 0;
-		// hitsText.text = hitsBaseText + "0";
+		_hitCounter = 0;
+		HitsText.text = HitsBaseText + "0";
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-	private void OnGUI()
-	{
-		GUI.Label(new Rect(10, 10, 100, 20), "hits: " + hitCounter);
-	}
 
 	void OnCollisionEnter(Collision collision)
 	{
-		hitCounter++;
-		// hitsText.text = hitsBaseText + hit_counter.ToString();
+		_hitCounter++;
+		HitsText.text = HitsBaseText + _hitCounter.ToString();
 	}
 }
