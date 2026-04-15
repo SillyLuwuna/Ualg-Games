43c1236be9ed46ccb4ff3319720d32e2fefaf18b
luna <rokdoombeast@gmail.com>
2026-04-15 20:54:00 +0100

    made code better

--- mancuna_ai/Mancuna/obj/Debug/net10.0/Mancuna.AssemblyInfo.cs
+++ mancuna_ai/Mancuna/obj/Debug/net10.0/Mancuna.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mancuna")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5352ae600bdc291b54e36f83796c3a53c78c8128")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d76e35a91054468e89c2371aed3c689a65f0a62")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mancuna")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mancuna")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- mancuna_ai/Mancuna/src/todo/PlayerMancuna.cs
+++ mancuna_ai/Mancuna/src/todo/PlayerMancuna.cs
@@ -7,22 +7,19 @@
 
 public class PlayerMancuna : Player
 {
-    private Dictionary<IBoard, Outcome> _cacheMax;
-    private Dictionary<IBoard, Outcome> _cacheMin;
+    private Dictionary<IBoard, Outcome> _cache;
 	private Dictionary<IBoard, ulong> _boardFrequency;
-	// private Dictionary<IBoard, double> _boardScores;
 	private ulong _maxDepth;
-	// private int _otherPlayer;
+	private int _otherPlayer;
+	private bool _firstPlay;
 
 	public PlayerMancuna(string name, int pos) : base(name, pos)
 	{
-		_cacheMax = new Dictionary<IBoard, Outcome>();
-		_cacheMin = new Dictionary<IBoard, Outcome>();
+		_cache = new Dictionary<IBoard, Outcome>();
 		_boardFrequency = new Dictionary<IBoard, ulong>();
-		// _boardScores = new Dictionary<IBoard, double>();
-		// _maxDepth = 11;
 		_maxDepth = ulong.MaxValue;
-		// _otherPlayer = GetOtherPlayer();
+		_otherPlayer = GetOtherPlayer(_position);
+		_firstPlay = true;
 	}
 
 	private enum Outcome
@@ -36,11 +33,7 @@ public class PlayerMancuna : Player
 
 	public override int play(IBoard board)
 	{ 
-		// Thread.Sleep(1000);
-		// _cacheMax.Clear();
-		// _cacheMin.Clear();
-		_boardFrequency.Clear();
-		// _otherPlayer = GetOtherPlayer();
+		_otherPlayer = GetOtherPlayer(_position);
 
 		int forcedPlay = board.forcedPlay();
 		if (forcedPlay != -1)
@@ -48,9 +41,7 @@ public class PlayerMancuna : Player
 			List<IBoard> children9;
 			List<int> play9;
 			(children9, play9) = board.children();
-			Outcome outcome9 = GetOutcome(children9[0]);
-			// Console.WriteLine("Option: " + outcome9.ToString());
-			// Console.WriteLine();
+			Outcome outcome9 = GetOutcome(children9[0], 0, true);
 			return forcedPlay;
 		}
 
@@ -58,15 +49,13 @@ public class PlayerMancuna : Player
 		List<int> play;
 		(children, play) = board.children();
 
-		Outcome outcome0 = GetOutcome(children[0]);
+		Outcome outcome0 = GetOutcome(children[0], 0, true);
 		if (outcome0 == Outcome.Win) return play[0];
 
-		Outcome outcome1 = GetOutcome(children[1]);
+		Outcome outcome1 = GetOutcome(children[1], 0, true);
 		if (outcome1 == Outcome.Win) return play[1];
 
-		// Console.WriteLine("Option 0: " + outcome0.ToString());
-		// Console.WriteLine("Option 1: " + outcome1.ToString());
-		// Console.WriteLine();
+		_firstPlay = false;
 
 		if (outcome0 == Outcome.Tie) return play[0];
 		if (outcome1 == Outcome.Tie) return play[1];
@@ -77,22 +66,17 @@ public class PlayerMancuna : Player
 		return play[0];
 	}
 
-	private Outcome GetOutcome(IBoard board)
-	{
-		return GetOutcome(board, 0, true);
-	}
-
 	private Outcome GetOutcome(IBoard board, ulong depth, bool isMin)
 	{
 		if (depth >= _maxDepth) return Outcome.Unknown;
 
-		UpdateFrequency(board);
-		if (IsTied(board)) return Outcome.Tie;
-
 		Outcome cacheResult = LoadFromCache(board, isMin);
 		if (cacheResult != Outcome.Invalid) return cacheResult;
 
-		Outcome boardOutcome = GetBoardOutcome(board);
+		if (_firstPlay) UpdateFrequency(board);
+		if (IsTied(board)) return Outcome.Tie;
+
+		Outcome boardOutcome = GetBoardOutcome(board, _position);
 		if (boardOutcome != Outcome.Unknown) return boardOutcome;
 
 		List<IBoard> children;
@@ -113,8 +97,6 @@ public class PlayerMancuna : Player
 		if (outcome1 == Outcome.Win && !isMin) return Outcome.Win;
 		if (outcome1 == Outcome.Lose && isMin) return Outcome.Lose;
 
-		// double outcome0Score = children[0].score();
-		// double outcome1Score = children[1].score();
 		if (outcome0 == Outcome.Tie || outcome1 == Outcome.Tie) return Outcome.Tie;
 		if (outcome0 == Outcome.Unknown || outcome1 == Outcome.Unknown) return Outcome.Unknown;
 
@@ -123,13 +105,10 @@ public class PlayerMancuna : Player
 
 	private void SaveToCache(IBoard board, Outcome outcome, bool isMin)
 	{
-		if (isMin && !_cacheMin.ContainsKey(board))
+		if (!_cache.ContainsKey(board))
 		{
-			_cacheMin.Add(board, outcome);
-		}
-		else if (!isMin && !_cacheMax.ContainsKey(board))
-		{
-			_cacheMax.Add(board, outcome);
+			if (isMin) outcome = ReverseOutcome(outcome);
+			_cache.Add(board, outcome);
 		}
 	}
 
@@ -137,13 +116,10 @@ public class PlayerMancuna : Player
 	{
 		Outcome result = Outcome.Invalid;
 
-		if (!isMin && _cacheMax.ContainsKey(board))
+		if (_cache.ContainsKey(board))
 		{
-			result = _cacheMax[board];
-		}
-		else if (isMin && _cacheMin.ContainsKey(board))
-		{
-			result = _cacheMin[board];
+			result = _cache[board];
+			if (isMin) result = ReverseOutcome(result);
 		}
 
 		return result;
@@ -163,18 +139,31 @@ public class PlayerMancuna : Player
 		return _boardFrequency[board] >= 3;
 	}
 
-	private Outcome GetBoardOutcome(IBoard board)
+	private Outcome GetBoardOutcome(IBoard board, int player)
 	{
 		int curr_outcome = board.winner();
 		if (curr_outcome == -2) return Outcome.Unknown;
 		if (curr_outcome == -1) return Outcome.Tie;
-		if (curr_outcome == _position) return Outcome.Win;
+		if (curr_outcome == player) return Outcome.Win;
 		return Outcome.Lose;
 	}
 
-	// private int GetOtherPlayer()
-	// {
-	// 	if (_position == 0) return 1;
-	// 	return 0;
-	// }
+	private int GetOtherPlayer(int player)
+	{
+		if (player == 0) return 1;
+		return 0;
+	}
+
+	private double GetBoardScore(IBoard board, int player)
+	{
+		return board.score(player) - board.score(_otherPlayer);
+	}
+
+	private Outcome ReverseOutcome(Outcome outcome)
+	{
+		Outcome reverseOutcome = outcome;
+		if (outcome == Outcome.Win) reverseOutcome = Outcome.Lose;
+		else if (outcome == Outcome.Lose) reverseOutcome = Outcome.Win;
+		return reverseOutcome;
+	}
 }
